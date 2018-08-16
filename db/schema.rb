8c94bc3accdd9b2cd2116feddd1d85d19b14a6b0
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 15:52:49 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816153649) do
+ActiveRecord::Schema.define(version: 20180816155030) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20180816153649) do
 
   create_table "songs", force: :cascade do |t|
     t.string  "name"
-    t.integer "atritst_id"
+    t.integer "artist_id"
   end
 
 end
