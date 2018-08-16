98b18c4c3158175c7a7dfc7519a7eb38ff51073e
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 17:33:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,13 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  patch '/songs/house-of-balloons' do
-    "Hello World"
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
   end
-  
+
 end
