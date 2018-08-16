1f080bf9bc2d0571916f3f795a706da980ed1375
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 16:00:04 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,13 +2,13 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, :through => :songs
 
-  def slug 
+  def slug
     name.downcase.gsub(" ","-")
-  end 
+  end
 
-  def self.find_by_slug(slugged) 
-    Artist.all.find do |artist| 
-      artist.slugged == slugged 
-    end 
+  def self.find_by_slug(slugged)
+    Artist.all.find do |artist|
+      artist.slugged == slugged
+    end
   end 
 end
