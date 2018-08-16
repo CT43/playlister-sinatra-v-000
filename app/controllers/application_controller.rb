351fcd5505ea66afea963a8baf3f1930312bf4be
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 17:40:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/songs/:slug' do
+    binding.pry
     @song = Song.find_by_slug(params[:slug])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
