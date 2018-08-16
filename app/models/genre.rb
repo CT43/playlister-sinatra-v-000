02595237c2c6fcd8d4830f55ecb7cf97a5857856
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 16:00:51 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,14 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, :through => :song_genres
   has_many :artists, :through => :songs
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slugged)
+    Genre.all.find do |genre|
+      genre.slug == slugged
+    end
+  end
 end
