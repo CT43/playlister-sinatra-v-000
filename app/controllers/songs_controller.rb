4f307d2f12efcc3ddd38f16b73e44872ee2e6429
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-16 17:13:07 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -31,7 +31,7 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.update(params["Name"])
+    @song.update(params[:song])
     if !params["Artist Name"].empty?
       @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     end
